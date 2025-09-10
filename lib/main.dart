fa27f3607ffe2fadaad73600fc81c2f9d2579be9
casipit22 <casipitcyrakim@gmail.com>
2025-09-11 01:03:46 +0800

    Delete lib directory

--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'students.dart';
-void main(){
-   Students students = Students('Cyra Casipit',19,'Naval Biliran',12345,'BSCS');
-   students.displayInfo();
-   students.setStudentsId(54321);
-   print('Updated Students ID: ${students.getStudentsId}');   
-   }
